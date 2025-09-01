b105fe4df7bf292ce0e6ada17cccf381202d3e0b
Gabriel Gallan <gabriel31.gal@gmail.com>
2025-09-01 15:40:10 -0300

    Route to GET Server Infos

--- Proxy/app.js
+++ Proxy/app.js
@@ -16,4 +16,17 @@ app.get('/proxy_api/v1/cameras/:cam_id/image/:time', async (request, reply) => {
     }
 })
 
+app.get('/proxy_api/server_info', async (request, reply) => {
+    try {
+        const data = {
+            method: 'http',
+            server_ip: request.socket.localAddress, // IP do servidor
+            server_port: request.socket.localPort   // Porta do servidor
+        }
+        reply.status(200).send({ status: 'success', data })
+    } catch (err) {
+        reply.status(404).send({ status: 'failed', message: err.message })
+    }
+})
+
 export default app
